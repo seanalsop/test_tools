74fe6234ff638d27af94df2588476a6dda5c0593
Sean Alsop <sean.alsop@d-tacq.co.uk>
2019-01-30 13:47:39 +0000

    Added isramp.c 	new file:   isramp.c

---
+++ isramp.c
@@ -0,0 +1,53 @@
+#include <unistd.h>
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+
+    unsigned xx;
+    int xx1 = 0;
+    unsigned long long ii = 1;
+    unsigned errors = 0;
+    unsigned error_report = 0;
+    unsigned int aa = 0;
+    char buffer[416];
+
+    while(1) {
+
+      //read(STDIN_FILENO, buffer, 416);
+      fread(buffer, sizeof(char), 416, stdin); // read 104 channels of data.
+      aa = *(int*)&buffer[384];
+
+      if (xx1 == aa) {
+         // printf("xx1 == aa \n");
+         // printf("%d , %d \n", xx1, aa );
+         //continue;
+      }
+      if (aa == xx1 + 1) {
+        //printf("Success! \n");
+        //printf("ii = %d \n", ii);
+        //printf("%d , %d \n", xx1, aa );
+        //continue;
+      } else {
+        if (aa != xx1 + 1){
+          if (++error_report < 100000){
+
+            printf("%012llx 0x%08x 0x%08x ** ERROR **\n",
+            ii, xx1, xx);
+            printf("Failed at sample number: %d \n", ii);
+            printf("Sample jump: %d \n \n", aa - xx1);
+            }
+          ++errors;
+        }else{
+
+          error_report = 0;
+        }
+      }
+
+      if (ii % 100000 == 0) {
+      printf("Loops completed = %d \n \n", ii);
+      }
+      ii++;
+      xx1 = aa;
+      }
+      return 0;
+}
