2863e489eecb07394ec8d6c2b63a60bedc8dc7ac
Sean Alsop <sean.alsop@d-tacq.co.uk>
2019-01-30 16:27:23 +0000

    Added args 	modified:   isramp.c

--- isramp.c
+++ isramp.c
@@ -1,33 +1,48 @@
 #include <unistd.h>
 #include <stdio.h>
 /*
- * This file takes input from nc as below: 
+ * This file takes input from nc as below:
  * nc acq2106_112 4210 | ./isramp
  */
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
 
+    int maxcols = 104; // Number of columns of data
+    int countcol = 96; // Column where the ramp is
     unsigned xx;
     int xx1 = 0;
     unsigned long long ii = 1;
     unsigned errors = 0;
     unsigned error_report = 0;
     unsigned int aa = 0;
-    char buffer[416];
+    unsigned buffer[104];
+    int opt;
+    while((opt = getopt(argc, argv, ":if:mc")) != -1)
+    {
+      switch(opt) {
+        case 'm':
+          maxcols = opt;
+          break;
+        case 'c':
+          countcol = opt;
+          break;
+        default:
+          printf("No args given %d \n", opt);
+          break;
+      }
+    }
 
     while(1) {
 
-      fread(buffer, sizeof(char), 416, stdin); // read 104 channels of data.
-      aa = *(int*)&buffer[384];
+      fread(buffer, sizeof(unsigned), 104, stdin); // read 104 channels of data.
+      aa = buffer[96];
 
       if (aa == xx1 + 1) {
       } else {
         if (aa != xx1 + 1){
           if (++error_report < 100000){
 
-            printf("%012llx 0x%08x 0x%08x ** ERROR **\n",
-            ii, xx1, xx);
-            printf("Failed at sample number: %d \n", ii);
-            printf("Sample jump: %d \n \n", aa - xx1);
+            printf("%012llx 0x%08x 0x%08x **ERROR** Sample jump: %d\n",
+            ii, xx1, aa, aa - xx1);
             }
           ++errors;
         }else{
