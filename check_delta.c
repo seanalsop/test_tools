93442df951af242f72869c4e30a47b6dcb857a2c
Sean Alsop <sean.alsop@d-tacq.co.uk>
2019-04-03 15:08:31 +0100

    General improvements. 	modified:   check_delta.c

--- check_delta.c
+++ check_delta.c
@@ -10,7 +10,8 @@ int main(int argc, char *argv[]) {
 
     int maxcols = 104; // Number of columns of data
     int countcol = 96; // Column where the ramp is
-    int delta = 1; // Default step. For sample counter in spad step = 1.
+    int lo_delta = 1; // Default step. For sample counter in spad step = 1.
+    long int hi_delta = 500000000; // 500,000,000
     unsigned xx;
     signed long int xx1 = 0;
     unsigned long long ii = 1;
@@ -19,21 +20,24 @@ int main(int argc, char *argv[]) {
     unsigned int aa = 0;
 
     int opt;
-    while((opt = getopt(argc, argv, "m:c:d:")) != -1)
+    while((opt = getopt(argc, argv, "m:c:l:h:")) != -1)
     {
       switch(opt) {
         case 'm':
           maxcols = atoi(optarg);
-          printf("%i\n", atoi(optarg));
+          printf("m = %i\n", atoi(optarg));
           break;
         case 'c':
           countcol = atoi(optarg);
-          printf("%i\n", atoi(optarg));
+          printf("c = %i\n", atoi(optarg));
           break;
-        case 'd':
-          delta = atoi(optarg);
-          printf("%i\n", atoi(optarg));
+        case 'l':
+          lo_delta = atoi(optarg);
+          printf("l = %i\n", atoi(optarg));
           break;
+        case 'h':
+          hi_delta = atoi(optarg);
+          printf("h = %i\n", atoi(optarg));
         default:
           printf("No args given %d \n", opt);
           break;
@@ -49,13 +53,14 @@ int main(int argc, char *argv[]) {
           xx1 = aa;
           continue;
       }
-      if (aa == xx1 + delta) {
+      if (aa == xx1 + lo_delta) {
       } else {
         //printf("Sample now: %d, sample before: %d, delta = %d \n", aa, xx1, aa - xx1);
-        if (abs(aa - xx1) < delta){
+
+        if (abs(aa - xx1) < lo_delta || abs(aa - xx1) > hi_delta){
           // if (aa == xx1) {
 
-          printf("Loop = %d, last sample = %d, current sample = %d, delta = %d \n" , ii, xx1, aa, delta);
+          printf("Loop = %d, last sample = %d, current sample = %d\n" , ii, xx1, aa);
           if (++error_report < 100000){
 
             printf("%012llx 0x%08x 0x%08x **ERROR** Sample jump: %d\n",
