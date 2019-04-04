458e73ca61f7737d01fe6085ee36dace4550ce3e
Sean Alsop <sean.alsop@d-tacq.co.uk>
2019-04-04 11:22:10 +0100

    Changed the size of the disco search window 	modified:   discos.py

--- discos.py
+++ discos.py
@@ -25,7 +25,6 @@ def run_disco_check(args):
         sample_counts = data[args.chans + 0::104]
         mux_pattern = data[args.chans + 3::104]
 
-
         for index, item in enumerate(ch):
             #print sample_counts[index], mux_changing[index], mux_pattern[index]
             if index == 0:
@@ -33,21 +32,28 @@ def run_disco_check(args):
             if item - ch[index - 1] > args.disco_size:
                 #print "Disco found at index: ", index
                 # print item - ch[index - 1]
-                for place, val in enumerate(mux_pattern[index - 1000 : index + 1000]):
+                for place, val in enumerate(mux_pattern[index - 100 : index + 100]):
                     if place == 0:
                         prev = val
                         continue
                     if val != prev:
                         #print "Mux has changed alongside mux change indicator."
                         change_flag = True
-                        if place < 500 or place > 1500:
-                            print "Larger than expected gap between disco and mux pattern: ", place
+                        # if place < 500 or place > 1500:
+                            # print "Larger than expected gap between disco and mux pattern: ", place
                         break
                     prev = val
 
                 if not change_flag:
                     print "ILLEGAL DISCONTINUITY FOUND."
-                    print item - ch[index - 1]
+                    print "disco: ", item - ch[index - 1]
+                    print "index: ", index
+                    print "place = ", place
+                    print "val, prev:", val, prev
+                    print "change_flag: ", change_flag
+
+                    ch.tofile("ch1.dat")
+                    mux_pattern.tofile("mux_pattern.dat")
                 else:
                     change_flag = False
 
