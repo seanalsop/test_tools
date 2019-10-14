f2798a1a8eb77b64f8a4cdbe06b49e4b88b98a0b
Sean <sean.alsop@d-tacq.co.uk>
2019-10-14 12:55:30 +0100

    Working version with normalised PSD. 	modified:   psd.py

--- stream/psd.py
+++ stream/psd.py
@@ -8,6 +8,7 @@ import argparse
 import acq400_hapi
 import concurrent.futures
 import sys
+from itertools import repeat
 
 
 def get_args():
@@ -22,7 +23,15 @@ def get_args():
 
 
 def perform_psd(channel, fs):
-    f, Pxx_den = signal.welch(channel, fs)
+
+    # Calculate the periodogram.
+    f, Pxx_den = signal.periodogram(channel, window='hanning')
+
+    # Normalise so we don't go over 0dB.
+    Pxx_den = Pxx_den / np.max(Pxx_den)
+
+    # Take the log so we can view it properly.
+    Pxx_den = 10*np.log10(Pxx_den)
     return [f, Pxx_den]
 
 
@@ -30,30 +39,40 @@ def main():
 
     args = get_args()
 
-    uut = acq400_hapi.Acq400(args.uut[0])
-    nchan = uut.get_ai_channels()
-    fs = round(float(uut.s0.SIG_CLK_MB_FREQ.split(" ")[1]))
-    _dtype = np.int32 if int(uut.s0.data32) else np.int16
+#    uut = acq400_hapi.Acq400(args.uut[0])
+#    print("1")
+#    nchan = uut.get_ai_channels()
+#    print("2")
+#    fs = round(float(uut.s0.SIG_CLK_MB_FREQ.split(" ")[1]))
+#    print("3")
+#    _dtype = np.int32 if int(uut.s0.data32) else np.int16
 
-    # Calculate the power spectral density
-    # rawdata = np.fromfile(args.file, dtype=_dtype)
+    nchan = 128
+    fs = 20000
+    _dtype = np.int16
 
     while True:
         data = []
         # read in 1M samples from stdin
-        rawdata = sys.stdin.read(1e6 * nchan)
-        rawdata = np.fromstring(data, dtype=_dtype)
-        for ch in range(0, nchan):
+        print("Reading data now")
+        rawdata = sys.stdin.buffer.read(int(1e5 * nchan))
+        print("Read data")
+        rawdata = np.fromstring(rawdata, dtype=_dtype)
+
+        for ch in range(0, 1):
             data.append(np.array(rawdata[ch::nchan]))
 
+        data[0] = data[0] / 32768 * 10
 
         with concurrent.futures.ThreadPoolExecutor() as executor:
             # Here we will get a thread per channel.
-            results = executor.map(perform_psd, data, fs)
+            results = executor.map(perform_psd, data, repeat(fs))
             for result in results:
-                plt.semilogy(result[0], result[1])
+                print(result[0], result[1])
+                plt.plot(result[0], result[1])
+            #plt.plot(data[0])
             plt.xlabel('frequency [Hz]')
-            plt.ylabel('PSD [V**2/Hz]')
+            plt.ylabel('Normalised PSD (dB)')
             plt.show()
 
 
